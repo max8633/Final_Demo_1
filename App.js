c35f2778a24de17cb8eaa092b803e271bad36eb7
s0461117 <as120450@gmail.com>
2017-12-27 13:18:06 +0800

    first commit

--- App.js
+++ App.js
@@ -1,54 +1,66 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+import { StyleSheet, Platform, Image, Text, View,ListView } from 'react-native';
 
 import firebase from 'react-native-firebase';
 
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "apiKey",
+  authDomain: "fireb-c693f.firebaseapp.com",
+  databaseURL: "https://fireb-c693f.firebaseio.com",
+  storageBucket: "fireb-c693f.appspot.com",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 export default class App extends React.Component {
-  constructor() {
-    super();
+
+    listenForItems(itemsRef) {
+        itemsRef.on('value', (snap) => {
+
+          // get children as an array
+          var items = [];
+          snap.forEach((child) => {
+            items.push({
+              title: child.val().title,
+              _key: child.key
+            });
+          });
+
+          this.setState({
+            dataSource: this.state.dataSource.cloneWithRows(items)
+          });
+
+        });
+      }
+
+  constructor(props) {
+    super(props);
+    this.itemsRef = firebaseApp.database().ref();
+
+    this.itemsRef.set({
+        title:'4488',
+        author:'sws'
+    });
+    this.itemsRef.update({ author: 'kar' });
+
     this.state = {
-      // firebase things?
+      dataSource: new ListView.DataSource({
+                  rowHasChanged: (row1, row2) => row1 !== row2,
+                })
     };
   }
 
   componentDidMount() {
-    // firebase things?
+    this.listenForItems(this.itemsRef);
   }
+
+
 
   render() {
     return (
-      <View style={styles.container}>
-        <Image source={require('./assets/RNFirebase512x512.png')} style={[styles.logo]} />
-        <Text style={styles.welcome}>
-          Welcome to the React Native{'\n'}Firebase starter project!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        {Platform.OS === 'ios' ? (
-          <Text style={styles.instructions}>
-            Press Cmd+R to reload,{'\n'}
-            Cmd+D or shake for dev menu
-          </Text>
-        ) : (
-          <Text style={styles.instructions}>
-            Double tap R on your keyboard to reload,{'\n'}
-            Cmd+M or shake for dev menu
-          </Text>
-        )}
-        <View style={styles.modules}>
-          <Text style={styles.modulesHeader}>The following Firebase modules are enabled:</Text>
-          {firebase.admob.nativeModuleExists && <Text style={styles.module}>Admob</Text>}
-          {firebase.analytics.nativeModuleExists && <Text style={styles.module}>Analytics</Text>}
-          {firebase.auth.nativeModuleExists && <Text style={styles.module}>Authentication</Text>}
-          {firebase.crash.nativeModuleExists && <Text style={styles.module}>Crash Reporting</Text>}
-          {firebase.firestore.nativeModuleExists && <Text style={styles.module}>Cloud Firestore</Text>}
-          {firebase.messaging.nativeModuleExists && <Text style={styles.module}>Messaging</Text>}
-          {firebase.perf.nativeModuleExists && <Text style={styles.module}>Performance Monitoring</Text>}
-          {firebase.database.nativeModuleExists && <Text style={styles.module}>Realtime Database</Text>}
-          {firebase.config.nativeModuleExists && <Text style={styles.module}>Remote Config</Text>}
-          {firebase.storage.nativeModuleExists && <Text style={styles.module}>Storage</Text>}
-        </View>
+      <View>
+
+
       </View>
     );
   }
--- android/app/src/main/java/com/ncue/rnfirebasestarter/MainApplication.java
+++ android/app/src/main/java/com/ncue/rnfirebasestarter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.invertase.rnfirebasestarter;
+package com.ncue.restaurant;
 
 import android.app.Application;
 
